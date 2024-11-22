8febe1260cca09ba8d9fa6f3aa425a3c01eb1e7e
Jifoltech <jeremiebo007@gmail.com>
2024-11-22 16:49:01 +0100

    First commit

---
+++ src/Components/navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav class="navbar">
+        <div class="container">
+          <a href="/" class="navbar-brand">
+            <img src="logo.png" alt="logo" />
+          </a>
+          <form class="d-flex">
+            <button class="btn" type="submit">
+              Get Started
+            </button>
+          </form>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
